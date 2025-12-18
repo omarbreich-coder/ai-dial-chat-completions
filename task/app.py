5981e56566b223f2c34d908be2cd65557abc7cc1
Omar Breich <Omar_Breich@epam.com>
2025-12-18 13:48:25 +0200

    Complete completions task

--- task/app.py
+++ task/app.py
@@ -8,26 +8,59 @@ from task.models.role import Role
 
 
 async def start(stream: bool) -> None:
-    #TODO:
+    # TODO:
     # 1.1. Create DialClient
     # (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
     #  you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
     #  don't forget to add your API_KEY)
+    dial_client = DialClient(deployment_name="gpt-4o")
     # 1.2. Create CustomDialClient
+    customDialClient = DialClient(deployment_name="gpt-4o")
+
     # 2. Create Conversation object
+    conversation = Conversation()
+
     # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
     #    messages.
+    print("Enter a system prompt or click 'enter' to continue")
+    system_prompt = input(">").strip()
+    if system_prompt:
+        conversation.add_message(Message(Role.SYSTEM, system_prompt))
+        print("System prompt added successfully")
+    else:
+        conversation.add_message(Message(Role.SYSTEM, DEFAULT_SYSTEM_PROMPT))
+        print(
+            f"No system prompt was provided so will fallback to the default prompt{DEFAULT_SYSTEM_PROMPT}"
+        )
+
     # 4. Use infinite cycle (while True) and get yser message from console
-    # 5. If user message is `exit` then stop the loop
-    # 6. Add user message to conversation history (role 'user')
-    # 7. If `stream` param is true -> call DialClient#stream_completion()
-    #    else -> call DialClient#get_completion()
-    # 8. Add generated message to history
-    # 9. Test it with DialClient and CustomDialClient
-    # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
-    raise NotImplementedError
+    while True:
+        user_input = input("Enter a message, otherwise type 'exit' to close the app\n")
+        # 5. If user message is `exit` then stop the loop
+        if user_input.lower() != "exit":
+            # 6. Add user message to conversation history (role 'user')
+            conversation.add_message(Message(Role.USER, user_input))
+            # 7. If `stream` param is true -> call DialClient#stream_completion()
+            #    else -> call DialClient#get_completion()
+            if stream:
+                ai_response = await customDialClient.stream_completion(
+                    conversation.get_messages()
+                )
+            else:
+                ai_response = customDialClient.get_completion(
+                    conversation.get_messages()
+                )
+            conversation.add_message(ai_response)
+            print("Ai response")
+            print(ai_response)
+
+        # 8. Add generated message to history
+
+        # 9. Test it with DialClient and CustomDialClient
+        # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
+        else:
+            print("Exiting the app")
+            break
 
 
-asyncio.run(
-    start(True)
-)
+asyncio.run(start(True))
--- task/clients/client.py
+++ task/clients/client.py
@@ -4,32 +4,61 @@ from task.clients.base import BaseClient
 from task.constants import DIAL_ENDPOINT
 from task.models.message import Message
 from task.models.role import Role
+from aidial_client import Dial, AsyncDial
 
 
 class DialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
-        #TODO:
+        # TODO:
         # Documentation: https://pypi.org/project/aidial-client/ (here you can find how to create and use these clients)
         # 1. Create Dial client
+
+        self._client = Dial(api_key=self._api_key, base_url=DIAL_ENDPOINT)
+
         # 2. Create AsyncDial client
+        self._async_client = AsyncDial(api_key=self._api_key, base_url=DIAL_ENDPOINT)
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
+        # TODO:
         # 1. Create chat completions with client
         #    Hint: to unpack messages you can use the `to_dict()` method from Message object
+        completion = self._client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            stream=False,
+            messages=[msg.to_dict() for msg in messages],
+        )
         # 2. Get content from response, print it and return message with assistant role and content
+        if res := completion.choices:
+            if message := res[0].message.content:
+                print(message)
+                return Message(Role.AI, message)
         # 3. If choices are not present then raise Exception("No choices in response found")
-        raise NotImplementedError
+        raise Exception("No choices in response found")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
+        # TODO:
         # 1. Create chat completions with async client
         #    Hint: don't forget to add `stream=True` in call.
+        chuncks = await self._async_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            stream=True,
+            messages=[msg.to_dict() for msg in messages],
+        )
+
         # 2. Create array with `contents` name (here we will collect all content chunks)
+        contents = []
         # 3. Make async loop from `chunks` (from 1st step)
         # 4. Print content chunk and collect it contents array
         # 5. Print empty row `print()` (it will represent the end of streaming and in console we will print input from a new line)
         # 6. Return Message with assistant role and message collected content
-        raise NotImplementedError
+
+        async for chunck in chuncks:
+            if chunck.choices:
+                delta = chunck.choices[0].delta
+                if delta and delta.content:
+                    print(delta.content)
+                    contents.append(delta.content)
+        print()
+        return Message(Role.AI, "".join(contents))
